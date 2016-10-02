96d3fce5210bcc3f8d40511f601f0ba7d6ccc5b6
Scotty Molt <smolten@users.noreply.github.com>
2016-10-01 17:54:19 -0700

    Simple Client/ Server
    Moving player, updated across both

---
+++ Networking Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿//https://unity3d.com/learn/tutorials/topics/multiplayer-networking/shooting-single-player?playlist=29690
+
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerController : NetworkBehaviour {	//MUST inherit from NetworkBehivour, not MonoBehaviour!
+
+	// Update is called once per frame
+	void Update () {
+
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+	}
+
+	//Do something for THIS local player on creation
+	//Useful for setting local camera angles or colors
+	public override void OnStartLocalPlayer ()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
+}
---
+++ README.txt
@@ -0,0 +1,4 @@
+A project used to test the networking/ multiplyar capabilities of Unity.
+
+Based on instructions/ information from:
+https://unity3d.com/learn/tutorials/topics/multiplayer-networking/introduction-simple-multiplayer-example?playlist=29690
